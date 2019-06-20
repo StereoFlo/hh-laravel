ffc8896647b4cf3737acab9aad2e454b1644ee99
StereoFlo <stereoflo@users.noreply.github.com>
2019-06-20 16:58:07 +0300

    fix

--- resources/views/weather/show.blade.php
+++ resources/views/weather/show.blade.php
@@ -8,7 +8,7 @@
             @foreach($city as $cityData)
             <h5 class="card-title">{{ !isset($cityData['created_at']) ?: $cityData['created_at'] }}</h5>
             <p class="card-text">
-                Temp: {{ isset($cityData['today_temp']) ?: $cityData['today_temp'] }},
+                Temp: {{ !isset($cityData['today_temp']) ?: $cityData['today_temp'] }},
                 Max temp: {{ !isset($cityData['today_max_temp']) ?: $cityData['today_max_temp'] }},
                 Min temp: {{ !isset($cityData['today_min_temp']) ?: $cityData['today_min_temp'] }}
             </p>
