221d9bb54563e087198c8e85a1d7dc7f6ebe9393
StereoFlo <stereoflo@users.noreply.github.com>
2019-06-20 14:45:54 +0300

    show template fix

--- resources/views/weather/show.blade.php
+++ resources/views/weather/show.blade.php
@@ -1,4 +1,18 @@
 @extends('layouts.app')
 @section('content')
-{{ dd($city) }}
+    <div class="card">
+        <div class="card-header">
+            {{ !isset($city[0]['city_name']) ?: $city[0]['city_name'] }}
+        </div>
+        <div class="card-body">
+            @foreach($city as $cityData)
+            <h5 class="card-title">{{ $cityData['created_at'] }}</h5>
+            <p class="card-text">
+                Temp: {{ $cityData['today_temp'] }},
+                Max temp: {{ $cityData['today_max_temp'] }},
+                Min temp: {{ $cityData['today_min_temp'] }}
+            </p>
+            @endforeach
+        </div>
+    </div>
 @endsection
