dc6ed8e2ded6f08e4010e8553b05d893428f158f
StereoFlo <stereoflo@users.noreply.github.com>
2019-06-20 18:53:24 +0300

    api prepare

--- app/Http/Middleware/ApiTokenCheck.php
+++ app/Http/Middleware/ApiTokenCheck.php
@@ -5,6 +5,8 @@ namespace App\Http\Middleware;
 use App\Models\User;
 use Carbon\Carbon;
 use Closure;
+use Exception;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 /**
@@ -24,13 +26,17 @@ class ApiTokenCheck
     public function handle($request, Closure $next)
     {
         if (empty($request->headers->get('x-api-token'))) {
-            abort(403);
+            return new JsonResponse(['error' => 'token is empty or expired'], 403);
         }
 
         $user = new User();
-        $user = $user->getByToken($request->headers->get('x-api-token'));
-        if (empty($user['token']) || empty($user['token_valid_until']) || Carbon::parse($user['token_valid_until'])->timestamp <= time()) {
-            abort(403);
+        try {
+            $user = $user->getByToken($request->headers->get('x-api-token'));
+            if (empty($user['token']) || empty($user['token_valid_until']) || Carbon::parse($user['token_valid_until'])->timestamp <= time()) {
+                return new JsonResponse(['error' => 'token is empty or expired'], 403);
+            }
+        } catch (Exception $exception) {
+            return new JsonResponse(['error' => 'token is not found'], 403);
         }
         return $next($request);
     }
